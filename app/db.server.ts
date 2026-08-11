61bb5459da41c5b32436f7ac1d326cde6470e54f
RonitShrestha1101 <ronit.shrestha1101@gmail.com>
2026-08-11 09:49:24 +0545

    server.ts

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,32 +1,21 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const uri = process.env.MONGODB_URI || "mongodb+srv://ronitshrestha1101_db_user:898EYEcetVfrjhjD@cluster0.yxihcio.mongodb.net/?appName=Cluster0";
 
 declare global {
-  var __dbClient: MongoClient | undefined;
+  var __mongooseConnection: typeof mongoose | undefined;
 }
-
-let client: MongoClient;
 
 if (process.env.NODE_ENV === "production") {
-  client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
+  mongoose.connect(uri);
 } else {
-  if (!globalThis.__dbClient) {
-    globalThis.__dbClient = new MongoClient(uri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
+  if (!globalThis.__mongooseConnection) {
+    globalThis.__mongooseConnection = mongoose;
+    mongoose.connect(uri);
   }
-  client = globalThis.__dbClient;
 }
 
-export { client };
+export { mongoose };
